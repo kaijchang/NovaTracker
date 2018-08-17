4393f3615507d3bba03ff6eca16138b5b2b73b9a
Kai Chang <kaijchang@gmail.com>
2018-08-17 11:40:55 -0700

    show turns and listen for file changes

--- renderer.js
+++ renderer.js
@@ -3,7 +3,7 @@ const path = require("path");
 const expandHomeDir = require("expand-home-dir");
 
 let game_folder;
-let logs;
+let log_folder;
 
 if (process.platform == "darwin") {
 	game_folder = expandHomeDir("~/Library/Application Support/com.dragonfoundry.novablitz");
@@ -11,20 +11,34 @@ if (process.platform == "darwin") {
 	// windows game files location
 }
 
-fs.readdirSync(game_folder).forEach(file => {
-	if (/^[A-Z0-9]{16}$/.test(file)) {
-		logs = JSON.parse(fs.readFileSync(path.join(game_folder, file, "game_log_file.json")));
-	}
+fs.readdirSync(game_folder).forEach(folder => {
+	if (/^[A-Z0-9]{16}$/.test(folder)) {
+		log_folder = folder;
+	} 
 });
 
-logs.reverse().forEach(game => {
-	var won = game["IsPlayerWinner"] ? 'Yes' : 'No';
-	$("tbody").append(`<tr>
-							<td>` + game["Format"] + `
-					   		<td>` + game["OpponentPlayerData"]["displayName"] + `</td>
-					   		<td><time class="timeago" datetime="` + game["StartTime"] + `">` + game["StartTime"] + `</time>
-					   		<td>` + won + `</td>
-					   </tr>`);
+
+function populateChart() {
+	$("tbody").empty();
+
+	var logs = JSON.parse(fs.readFileSync(path.join(game_folder, log_folder, "game_log_file.json")));
+	
+	logs.reverse().forEach(game => {
+		var won = game["IsPlayerWinner"] ? 'Yes' : 'No';
+		$("tbody").append(`<tr>
+								<td>` + game["Format"] + `
+						   		<td>` + game["OpponentPlayerData"]["displayName"] + `</td>
+						   		<td><time class="timeago" datetime="` + game["StartTime"] + `">` + game["StartTime"] + `</time>
+						   		<td>` + won + `</td>
+						   		<td>` + game["LogElements"][game["LogElements"].length - 1]["Turn"] + `</td>
+						   </tr>`);
+	});
+	
+	$("time.timeago").timeago();
+}
+
+fs.watchFile(path.join(game_folder, log_folder, "game_log_file.json"), (curr, prev) => {
+	populateChart();
 });
 
-$("time.timeago").timeago();
+populateChart();
