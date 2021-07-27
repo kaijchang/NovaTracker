025cad49eba79173b863d46fe0bfe7b95cb5d609
Kai Chang <kaijchang@gmail.com>
2021-07-26 22:18:58 -0700

    deploy script

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,6 +11,10 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 	},
+}
+
+if (process.env.SVELTE_BUILD_STATIC) {
+	config.kit.adapter = adapter({})
 }
 
 export default config
